315f40f3dd7e2d395f1398873900605f65828d79
akisinn <45202458+akisinn@users.noreply.github.com>
2026-02-21 17:20:06 +0900

    Add files via upload

--- souba/dist/js/myjs.js
+++ souba/dist/js/myjs.js
@@ -7,9 +7,6 @@
 	document.getElementById('check1').addEventListener('click', function() {
 		check1();
 	});
-	document.getElementById('check2').addEventListener('click', function() {
-		check2();
-	});
 
 	document.getElementById('reset').addEventListener('click', function() {
 		reset();
@@ -32,15 +29,6 @@
 		var keyword3 = document.getElementById('keyword3').value;
 		var keyword = keyword1 + ' ' + keyword2 + ' ' + keyword3;
 		var url = 'https://aucfan.com/search1/q-~' + toEUC(keyword) + '/s-mc/?o=de&itemstatusmc%5B%5D=used10&itemstatusmc%5B%5D=used20&itemstatusmc%5B%5D=used40&itemstatusmc%5B%5D=used60&sellertype=i&shipping=free&shopid=';
-		window.open(url);
-	}
-
-	function check2() {
-		var keyword1 = document.getElementById('keyword1').value;
-		var keyword2 = document.getElementById('keyword2').value;
-		var keyword3 = document.getElementById('keyword3').value;
-		var keyword = keyword1 + ' ' + keyword2 + ' ' + keyword3;
-		var url = 'https://aucfan.com/search1/q-~' + toEUC(keyword) + '/s-ya/?o=t1&itemstatus=used&sellertype=i&location=0';
 		window.open(url);
 	}
 
